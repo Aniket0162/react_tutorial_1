77f3b1b5d6ecaf5fd00d83051f3a041022aadc3a
Roronoa786Zoro <aniket199mistry@gmail.com>
2023-07-12 13:36:16 +0530

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react'
-
+import logo from './logo.svg';
+import './App.css';
 const App = () => {
   return (
+
+
     <div>
-      <h2>Day 3 Java Refrecer</h2>
-        <p>
-          Changes are done in index.html by creating the index file.
-        </p>
-      
-    </div>
+    <h3>Downloaded all the requied application to run the react code like</h3>
+    <p>
+      1. Downloaded Node Module for running javaScript with out a browser.
+      2. Downloaded Vs code Editior for performing code.
+      3. Then Downloaded react dev tool. React Dev Tools is an extension created by the React team. It enables developers to debug their code inside their Developer Tools. Once you add the extension, you will get 2 new tabs in DevTools called Components and Profiler, respectively. To use the full functionality of the extension, you need to be in development mode.
+
+      4. then we open vs code Downloaded some extension that will help to perform react app fast and efficient.
+          a. Prettier.
+          b. Bracket pair colorizor.
+          c. Auto renamer .
+          d. Es7 react redux library.
+          e. Thunder Client.
+
+    </p>
+    
+  </div>
   )
 }
 
-export default App
+export default App;
